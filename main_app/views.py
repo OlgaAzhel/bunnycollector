f504a4bf8f3e8b81849e5e62321ee2bfa6a1c6f6
Olga Azhel <olga.azhel@gmail.com>
2023-03-13 18:41:34 -0400

    add basic structure of the app

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+bunnies = [
+    {'name': 'Cinnabun', 'breed': 'Holland Lop',
+        'description': 'adorable bundle of joy', 'age': 3},
+    {'name': 'Truffle', 'breed': 'Lionhead',
+        'description': 'energetic little fluff ball', 'age': 2},
+]
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+def bunnies_index(request):
+    return render(request, 'bunnies/index.html', {'bunnies': bunnies})
